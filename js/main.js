51a05b010f29b59ba8ad1524347503fc04aa8811
tushar <tusharsubhrabose@gmail.com>
2025-03-27 11:05:42 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -119,20 +119,7 @@ $(function () {
     anchor scroll
 
     ***************************/
-    $(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
-
-        var target = $($.attr(this, 'href'));
-        var offset = 0;
-
-        if ($(window).width() < 1200) {
-            offset = 90;
-        }
-
-        $('html, body').animate({
-            scrollTop: target.offset().top - offset
-        }, 400);
-    });
+    
     /***************************
 
     append
